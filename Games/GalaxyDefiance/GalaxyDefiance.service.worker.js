7ad6f2b28e25238d4178d68cbc33332f0cc8c884
Hamza <149720326+HamzaWakkas@users.noreply.github.com>
2024-11-18 13:09:57 -0800

    PWA worked reexport without debug

--- Games/GalaxyDefiance/GalaxyDefiance.service.worker.js
+++ Games/GalaxyDefiance/GalaxyDefiance.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1731963908|194045302';
+const CACHE_VERSION = '1731964149|46572358';
 /** @type {string} */
 const CACHE_PREFIX = 'Galaxy Defiance-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
